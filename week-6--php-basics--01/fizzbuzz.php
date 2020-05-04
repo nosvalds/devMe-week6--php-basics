d68230f298582da16c8f62e47e32c7b15f23851b
Nik Osvalds <nosvalds@gmail.com>
2020-05-04 11:52:50 +0100

    fizzbuzz

--- week-6--php-basics--01/fizzbuzz.php
+++ week-6--php-basics--01/fizzbuzz.php
@@ -1,9 +1,10 @@
 <?php
 
-for ($$i = 1; $$i <= 100; $$i += 1) { 
+for ($i = 1; $i <= 100; $i += 1) { 
     $result = ($i%3 === 0) ? "Fizz" : "";
-    $result += ($i%5 === 0) ? "Buzz" : "";
+    $result .= ($i%5 === 0 ) ? "Buzz" : "";
     // $result += ($i%7 === 0) ? "Splat" : "";
     // $result += ($i%11 === 0) ? "Boom" : "";
-    var_dump(($result !== "") ? $result : $i); 
+    echo ($result !== "") ? $result : $i; 
+    echo "\n";
 }
