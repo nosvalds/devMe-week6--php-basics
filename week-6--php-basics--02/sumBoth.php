96d57bd64d78404dc18a157313f741ebde6725ae
Nik Osvalds <nosvalds@gmail.com>
2020-05-04 16:08:34 +0100

    sumBoth function complete

--- week-6--php-basics--02/sumBoth.php
+++ week-6--php-basics--02/sumBoth.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-function sum(array $numbers) : float {
+function sum(array $numbers) : int {
     $sum = 0;
     foreach ($numbers as $num) {
         $sum += $num;
@@ -11,10 +11,11 @@ function sum(array $numbers) : float {
     return $sum;
 }
 
-var_dump(sum([2, 3, 4, 5, 6]));
+function sumBoth(array $numbers1, array $numbers2) : int {
+    return sum($numbers1) + sum($numbers2);
+}
 
-// var_dump(
-//     sum([2, 3, 4, 5, 6]),
-//     sumBoth([2, 3, 4, 5, 6], [1, 2, 3, 4]), // int(30)
-//     sumBoth([2, 3, 4, 5, 6], [5, 10, 12]), // int(47)
-// );
+var_dump(
+    sumBoth([2, 3, 4, 5, 6], [1, 2, 3, 4]), // int(30)
+    sumBoth([2, 3, 4, 5, 6], [5, 10, 12]), // int(47)
+);
