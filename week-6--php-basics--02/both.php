d865948b7f1dfc04b9b4792b188e0ec73aa5ab29
Nik Osvalds <nosvalds@gmail.com>
2020-05-04 16:26:14 +0100

    both working with array_unique()

---
+++ week-6--php-basics--02/both.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+function both(array $nums1, array $nums2) : array {
+    $result = [];
+
+    foreach ($nums1 as $num1) {
+        foreach ($nums2 as $num2) {
+            if ($num1 === $num2) {
+                $result[] = $num1;
+            }
+        }
+    }
+
+    return array_unique($result);
+}
+
+var_dump(
+    both([2, 3, 4, 5, 6], [1, 2, 5, 6]), // [2, 5, 6]
+);
