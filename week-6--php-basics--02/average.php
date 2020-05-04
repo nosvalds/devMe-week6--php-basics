d0b2a1ad8a1c03da1441f406d8f043073421d10c
Nik Osvalds <nosvalds@gmail.com>
2020-05-04 15:15:54 +0100

    average function complete

---
+++ week-6--php-basics--02/average.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+function average(array $values) : float {
+    $sum = 0;
+
+    foreach ($values as $i => $value) {
+        $sum += $value;
+    }
+        
+    return $sum / count($values);
+}
+
+
+var_dump(
+    average([2, 3, 4, 5, 6]), // float(4)
+    average([2, 3]), // float(2.5)
+    average([10, 30]), // float(20)
+    average([-4, -8, -9]), // float(-7)
+);
