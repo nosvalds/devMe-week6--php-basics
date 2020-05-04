b2bf0cccc5589abdacde096ddf90e8a370431fd1
Nik Osvalds <nosvalds@gmail.com>
2020-05-04 15:21:49 +0100

    double function complete

---
+++ week-6--php-basics--02/double.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+function double(array $numbers) : array {
+    foreach ($numbers as $key => $num) {
+        $result[] = $num * 2;
+    }
+
+    return $result;
+}
+
+var_dump(
+    double([2, 3, 4, 5, 6]), // [4, 6, 8, 10, 12]
+    double([1, 2, 5]), // [2, 4, 10]
+);
