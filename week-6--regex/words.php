fa5cd3287f0e249b25585190f5047835729eb178
Nik Osvalds <nosvalds@gmail.com>
2020-05-05 10:15:42 +0100

    exercise 5 regex

---
+++ week-6--regex/words.php
@@ -0,0 +1,8 @@
+<?php
+
+declare(strict_types=1);
+
+// /\w*/
+
+var_dump(words("This is a sentence")); // ["This", "is", "a", "sentence"]
+var_dump(words("Hello, my name is Dolemite")); // ["Hello", "my", "name", "is", "Dolemite"]
