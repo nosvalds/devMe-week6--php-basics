20f0647e65ab042899f9ce3107203899932df9be
Nik Osvalds <nosvalds@gmail.com>
2020-05-05 16:53:10 +0100

    06 Validator complete

---
+++ week-6--classes/validator_06.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+class Validator 
+{
+    // methods
+
+    // email validation
+    public function email(string $email) : bool 
+    {
+        return filter_var($email, FILTER_VALIDATE_EMAIL) === $email; // validate email 
+    }
+
+    // postcode validation
+    public function postcode(string $postCode) : bool
+    {
+        return preg_match("/^[A-Z]{1,2}\d{1,2}[A-Z]? \d[A-Z]{2}$/", $postCode) === 1;
+    }
+
+}
+
+$validator = new Validator();
+
+// it validates email addresses
+echo "Email\n";
+var_dump($validator->email("alfonso@example.com")); // bool(true)
+var_dump($validator->email("wombat+12@spoons.plumbing")); // bool(true)
+var_dump($validator->email("blah blah blah! alfonso@example.com")); // bool(false)
+var_dump($validator->email("wombat@spoonsplumbing")); // bool(false)
+var_dump($validator->email("wombatspoons")); // bool(false)
+var_dump($validator->email("@wombatspoons")); // bool(false)
+
+// it validates postcodes
+echo "\nPostcode\n";
+var_dump($validator->postcode("BS4 3UH")); // bool(true)
+var_dump($validator->postcode("S10 4GR")); // bool(true)
+var_dump($validator->postcode("BS14 9DI")); // bool(true)
+var_dump($validator->postcode("SW1A 1AA")); // bool(true)
+var_dump($validator->postcode("12B DI9")); // bool(false)
+var_dump($validator->postcode("EST4 DD29")); // bool(false)
+var_dump($validator->postcode("blah blah BS5 8RJ blah blah")); // bool(false)
