d06a19a6b8c8db69870e56495fac1e73bf470ecc
Nik Osvalds <nosvalds@gmail.com>
2020-05-05 09:55:26 +0100

    setup exercise 1 and add Regex

--- class_exercises/regex/regex.php
+++ class_exercises/regex/regex.php
@@ -1,3 +1,11 @@
 <?php
 
-// Start Here
+declare(strict_types=1);
+
+function hasNumber(string $str) : bool {
+    // RegEx: /[0-9]/
+}
+
+var_dump(hasNumber("blah")); // bool(false)
+var_dump(hasNumber("bl3h")); // bool(true)
+var_dump(hasNumber("bl3h5")); // bool(true)
