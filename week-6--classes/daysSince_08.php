33c7dba5c1ca8405db48297d29f784080461edea
Nik Osvalds <nosvalds@gmail.com>
2020-05-06 09:23:45 +0100

    08 daysSince complete

--- week-6--classes/daysSince_08.php
+++ week-6--classes/daysSince_08.php
@@ -3,10 +3,13 @@
 declare(strict_types=1);
 
 function daysSince(string $date) : int {
-    // … your code here
+    $now = new DateTime(); // get todays date
+    $then = new DateTime($date); // convert input to DateTime object
+    $result = date_diff($now, $then); // diff them
+    return $result->days; // pull out "days" property
 }
 
 var_dump(
     daysSince("1984-04-16"), // 13 thousand and something
-    // why not try *your* date of birth?
+    daysSince("1989-03-08"),// why not try *your* date of birth?
 );
